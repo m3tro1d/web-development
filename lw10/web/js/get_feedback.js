f6fdb6b773d9fc2e372fe57409331ca4c79f7bca
m3tro1d <qazwf734@gmail.com>
2021-05-04 11:10:25 +0300

    lw10: more refactoring

--- lw10/web/js/get_feedback.js
+++ lw10/web/js/get_feedback.js
@@ -1,6 +1,55 @@
 const form = document.getElementById('feedback-form');
 const infoBlock = document.getElementById('feedback-result');
 
+
+function main() {
+  form.addEventListener('submit', handleFeedbackRequest);
+}
+
+async function handleFeedbackRequest(event) {
+  event.preventDefault();
+
+  const requestUri = '/feedbacks.php';
+  const options = getOptions(form);
+
+  try {
+    const feedback = await fetchFeedback(requestUri, options);
+    displayResult(feedback);
+  } catch (error) {
+    displayError(error.message);
+  }
+}
+
+function getOptions(form) {
+  const body = {
+    email: form.email.value,
+  };
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }
+}
+
+async function fetchFeedback(url, options) {
+  let response = null;
+  try {
+    response = await fetch(url, options);
+  } catch (e) {
+    throw new Error('Ошибка запроса: ' + e.message);
+  }
+  if (response) {
+    const json = await response.json();
+    if (response.ok) {
+      return json;
+    } else {
+      throw new Error(json.msg);
+    }
+  }
+}
+
 function displayResult(feedback) {
   infoBlock.classList.remove('feedback-result_error');
   let feedbackString = '';
@@ -15,30 +64,4 @@ function displayError(message) {
   infoBlock.textContent = message;
 }
 
-window.onload = function() {
-  async function getFeedback(event) {
-    event.preventDefault();
-
-    const feedbackUri = '/feedbacks.php';
-    const body = {
-      email: this.email.value,
-    };
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    };
-
-    const response = await fetch(feedbackUri, options);
-    const json = await response.json();
-    if (response.ok) {
-      displayResult(json);
-    } else {
-      displayError(json.msg);
-    }
-  }
-
-  form.addEventListener('submit', getFeedback);
-}
+window.onload = main;
